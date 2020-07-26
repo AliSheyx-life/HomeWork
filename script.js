50376b8dce27068d7318e58b2f40aca984fb1a02
AliSheyx-life <68541818+AliSheyx-life@users.noreply.github.com>
2020-07-26 19:54:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -11,25 +11,25 @@ function weeks() {
         weeksAnswer.style.color = 'red';
     } else if (weeksInput == 1) {
         weeksAnswer.innerHTML = 'Monday';
-        weeksAnswer.style.color = '#6ab04c';
+        weeksAnswer.style.color = 'yellow';
     } else if(weeksInput == 2) {
         weeksAnswer.innerHTML = 'Tuesday';
-        weeksAnswer.style.color = '#6ab04c';
+        weeksAnswer.style.color = 'yellow';
     } else if (weeksInput == 3) {
         weeksAnswer.innerHTML = 'Wednesday';
-        weeksAnswer.style.color = '#6ab04c';
+        weeksAnswer.style.color = 'yellow';
     } else if (weeksInput == 4) {
         weeksAnswer.innerHTML = 'Thursday';
-        weeksAnswer.style.color = '#6ab04c';
+        weeksAnswer.style.color = 'yellow';
     } else if (weeksInput == 5) {
         weeksAnswer.innerHTML = 'Friday';
         weeksAnswer.style.color = '#6ab04c';
     } else if (weeksInput == 6 ) {
         weeksAnswer.innerHTML = 'Saturday';
-        weeksAnswer.style.color = '#6ab04c';
+        weeksAnswer.style.color = 'yellow';
     } else if (weeksInput == 7) {
         weeksAnswer.innerHTML = 'Sunday';
-        weeksAnswer.style.color = '#6ab04c'
+        weeksAnswer.style.color = 'yellow'
     } else{
         weeksAnswer.innerHTML = 'В неделе 7 дней';
         weeksAnswer.style.color = '#fff'
@@ -52,19 +52,19 @@ function numbers() {
     
     if (numbersInput1 > numbersInput2 && numbersInput1 > numbersInput3) {
         numbersAnswer.innerHTML = numbersInput1;
-        numbersAnswer.style.color = '#6ab04c'
+        numbersAnswer.style.color = 'yellow'
     } else if (numbersInput2 > numbersInput1 && numbersInput2 > numbersInput3) {
         numbersAnswer.innerHTML = numbersInput2;
-        numbersAnswer.style.color = '#6ab04c'
+        numbersAnswer.style.color = 'yellow'
     } else if (numbersInput3 > numbersInput1 && numbersInput3 > numbersInput2) {
         numbersAnswer.innerHTML = numbersInput3;
-        numbersAnswer.style.color = '#6ab04c'
+        numbersAnswer.style.color = 'yellow'
     } else if (numbersInput1 == '' || numbersInput2 == '' || numbersInput3 == '') {
         numbersAnswer.innerHTML = 'Заполните все строчки';
         numbersAnswer.style.color = 'red';
     } else{
         numbersAnswer.innerHTML = 'Они равны';
-        numbersAnswer.style.color = '#6ab04c'
+        numbersAnswer.style.color = 'yellow'
     }
 }
 // ------------
@@ -101,7 +101,7 @@ function olds() {
         
     } else if (oldsInput == 0 || oldsInput < 7 && oldsInput > 0) {
         oldsAnswer.innerHTML = 'Вам ещё рости и рости';
-        oldsAnswer.style.color = '#6ab04c';
+        oldsAnswer.style.color = 'yellow';
         oldsImg1.style.opacity = '1';
         
         // 
@@ -113,7 +113,7 @@ function olds() {
         
     } else if (oldsInput > 7 && oldsInput < 19) {
         oldsAnswer.innerHTML = 'Вам ещё учиться и учиться';
-        oldsAnswer.style.color = '#6ab04c';
+        oldsAnswer.style.color = 'yellow';
         oldsImg2.style.opacity = '1';
                 
         // 
@@ -125,7 +125,7 @@ function olds() {
         
     } else if (oldsInput > 18 && oldsInput < 61) {
         oldsAnswer.innerHTML = 'Вам ещё работать и работать';
-        oldsAnswer.style.color = '#6ab04c';
+        oldsAnswer.style.color = 'yellow';
         oldsImg3.style.opacity = '1';
                 
         // 
@@ -137,7 +137,7 @@ function olds() {
         
     } else if (oldsInput > 60 && oldsInput < 101) {
         oldsAnswer.innerHTML = 'Вам уже на пенсию надо';
-        oldsAnswer.style.color = '#6ab04c';
+        oldsAnswer.style.color = 'yellow';
         oldsImg4.style.opacity = '1';
                 
         // 
@@ -149,7 +149,7 @@ function olds() {
         
     } else if (oldsInput > 100) {
         oldsAnswer.innerHTML = 'O_O';
-        oldsAnswer.style.color = '#6ab04c';
+        oldsAnswer.style.color = 'yellow';
         oldsImg5.style.opacity = '1'
                 
         // 
